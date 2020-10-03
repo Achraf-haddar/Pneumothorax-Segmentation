ce67fb644b05868b15a5f5f2247defcbfe9519b8
Achraf-haddar <achraf.haddar@supcom.tn>
2020-10-03 22:26:39 +0200

    to github

--- dataset.py
+++ dataset.py
@@ -20,7 +20,10 @@ from albumentations import(
     ShiftScaleRotate
 )
 
+import config
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+TRAIN_PATH = "Dataset/stage_2_train.csv"
 
 class SIMDataset(Dataset):
     def __init__(self, image_ids, transform=True, preprocessing_fn=None):
@@ -30,24 +33,84 @@ class SIMDataset(Dataset):
         :param transform: True/False, no transform in validation
         :param preprocessing_fn: a function for preprocessing image
         """
-        # 
+        # Create empty dictionary to store image and mask paths
+        self.data = defaultdict(dict)
+        # for augmentations
+        self.transform = transform
+        # preprocessing function to normalize images
+        self.preprocessing_fn = preprocessing_fn
+        # albumentations augmentations
+        self.aug = Compose(
+            [
+                ShiftScaleRotate(
+                    shift_limit=0.0625,
+                    scale_limit=0.1,
+                    rotate_limit=10,
+                    p=0.8
+                ),
+                OneOf(
+                    [
+                        RandomGamma(
+                            gamma_limit=(90, 110)
+                        ),
+                        RandomBrightnessContrast(
+                            brightness_limit=0.1,
+                            contrast_limit=0.1
+                        ),
+                    ],
+                    p=0.5,
+                ),
+            ]
+        )
 
+        # going over all image_ids to store image and mask paths
+        counter = 0
+        for imgid in image_ids:
+            files = glob.glob(os.path.join(config.TRAIN_PATH, imgid, "*.png"))
+            self.data[counter] = {
+                "img_path": os.path.join(
+                    config.TRAIN_PATH, imgid + ".png"
+                ),
+                "mask_path": os.path.join(
+                    config.TRAIN_PATH, imgid + "_mask.png"
+                )
+            }
+            counter += 1 
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, item):
+        # for a given item index return image and mask tensors
+        # read image and mask paths
+        img_path = self.data[item]["img_path"]
+        mask_path = self.data[item]["mask_path"]
 
+        # read image and convert to RGB
+        img = Image.open(img_path)
+        img = img.convert("RGB")
 
+        # PIL image to numpy array
+        img = np.array(img)
 
+        # read mask image
+        mask = Image.open(mask_path)
 
+        # convert to binary float matrix
+        mask = (mask >= 1).astype("float32")
+        
+        # if this is training data, apply transforms
+        if self.transfrom is True:
+            augmented = self.aug(image=img, mask=mask)
+            img = augmented["image"]
+            mask = augmented["mask"]
+        
+        # preprocess the image using provided preprocessing tensors
+        # image normalization
+        img = self.preprocessing_fn(img)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # return image and mask tensors
+        return {
+            "image": transforms.ToTensor()(img),
+            "mask": transforms.ToTensor()(mask).float()
+        }
